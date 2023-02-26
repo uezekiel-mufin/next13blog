45996e4b7c879258b99c6f99b83916b78491ce7d
Udiomuno Ezekiel <geniuswealthb@gmail.com>
2023-02-26 21:12:49 +0100

    setup project with nextjs, tailwindcss, typescript and sanityio

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+	return (
+		<div>
+			<h1 className='text-red-500 '>This is the HomePage</h1>
+			<h3>Welcome the ezzy blog</h3>
+		</div>
+	);
+};
+
+export default HomePage;
